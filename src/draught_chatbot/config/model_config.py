720be88ebdbebd24a9812c75c9dfb81905ac9f11
xingmo <xingmo_scut@163.com>
2024-10-31 13:02:11 +0800

    add login and cookie

--- src/draught_chatbot/config/model_config.py
+++ src/draught_chatbot/config/model_config.py
@@ -3,8 +3,8 @@ import os
 
 # support models 
 SUPPORT_MODEL_DICT = {
-    "zhipu": ["GLM-4-plus","GLM-4-0520","GLM-4-flashX","GLM-4-flash", "glm-4",],
     "doubao": ["Doubao-pro-32k", ],
+    "zhipu": ["GLM-4-flashX","GLM-4-plus","GLM-4-0520","GLM-4-flash", "glm-4",],
     "qianfan": ["ERNIE-Speed-128K", "ERNIE-Speed-8K", "ERNIE Speed-AppBuilder", "ERNIE-Lite-8K", "ERNIE-Lite-8K-0922", "ERNIE-Tiny-8K", "Yi-34B-Chat",],
     "zhipu-search": ["web-search-pro", ],
 }
